1a01fa1cde17de9460825fbf8da29a02a7af401c
Matvey Podgornov <elgarat@yahoo.com>
2015-09-17 16:11:22 +0300

    init folders, audio, meshes

---
+++ src/Assets/Editor/FBX_Import.cs.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class FBX_Import : AssetPostprocessor {
+
+	public const float importScale = 100.0f;
+
+	void OnPreprocessModel(){
+		ModelImporter importer = assetImporter as ModelImporter;
+		importer.globalScale = importScale;
+	}
+}
